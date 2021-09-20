fb466746342d1c6b090ae425831f17ed3a2550f9
lcroy <cl@m-tech.aau.dk>
2021-09-20 14:51:58 +0200

    commit new scripts

---
+++ gpt_neo_fine_tune.py
@@ -0,0 +1,70 @@
+import torch
+import logging
+from torch.utils.data import Dataset, random_split
+from transformers import GPT2Tokenizer, TrainingArguments, Trainer, GPTNeoForCausalLM
+from config import Config
+from tqdm import tqdm, trange
+
+logger = logging.getLogger(__name__)
+
+torch.manual_seed(22)
+
+class IROWData(Dataset):
+    def __init__(self, lines, tokenizer, max_length):
+        self.input_ids = []
+        self.attn_masks = []
+        self.labels = []
+        for line in lines:
+            encodings_dict = tokenizer(line, truncation=True,
+                                       max_length=max_length, padding="max_length")
+            self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
+            self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.attn_masks[idx]
+
+
+def train(dataset, model, output_path, log_path):
+    # split the dataset to training and validation
+    total_num_dataset = len(dataset)
+    train_size = int(0.8 * total_num_dataset)
+    val_size = total_num_dataset - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    # set up the training arguments
+    training_args = TrainingArguments(output_dir=output_path, num_train_epochs=5, logging_steps=5, save_steps=100,
+                                      per_device_train_batch_size=1, per_device_eval_batch_size=1,
+                                      warmup_steps=100, weight_decay=0.01, logging_dir=log_path)
+    trainer = Trainer(model=model, args=training_args, train_dataset=train_dataset,
+            eval_dataset=val_dataset, data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data]),
+                                                                  'attention_mask': torch.stack([f[1] for f in data]),
+                                                                  'labels': torch.stack([f[0] for f in data])})
+    # start training and evaluation
+    print("Start Training...")
+    trainer.train()
+    logger.info("We are saving model checkpoint to %s", output_path)
+
+
+def main():
+    cfg = Config()
+    # initial gpt-neo model and tokenizer
+    model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B").cuda()
+    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B", bos_token='<|endoftext|>',
+                                              eos_token='<|endoftext|>', pad_token='<|pad|>')
+    model.resize_token_embeddings(len(tokenizer))
+    tokenizer.save_pretrained(cfg.model_gpt2_neo_checkpoint_path)
+
+    #load the dataset
+    with open(cfg.dataset_path_IR_delex, encoding="utf-8") as f:
+        lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+    # formate the datset
+    dataset = IROWData(lines, tokenizer, cfg.max_length)
+
+    #Training and evaluation
+    train(dataset, model, cfg.model_gpt2_neo_checkpoint_path,cfg.model_gpt2_neo_log_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ gpt_neo_gen.py
@@ -0,0 +1,246 @@
+from config import Config
+# from utils.speech_service import *
+from utils.dbsearch import *
+from utils.normalize_text import normalize
+
+import torch
+from transformers import GPT2Tokenizer, GPTNeoForCausalLM
+
+cfg = Config()
+
+def db_search(context_pred_belief):
+
+    loc_db_req = context_pred_belief.find('<|DB_req|>')
+    loc_db_opt = context_pred_belief.find('<|DB_opt|>')
+    loc_t_req = context_pred_belief.find('<|T_req|>')
+    loc_t_opt = context_pred_belief.find('<|T_opt|>')
+    loc_eob = context_pred_belief.find('<|eob|>')
+    # get db_req slots
+    if loc_db_req > 0:
+        if loc_db_opt > 0:
+            db_req = context_pred_belief[loc_db_req + 10:loc_db_opt]
+        elif loc_t_req > 0:
+            db_req = context_pred_belief[loc_db_req + 10:loc_t_req]
+        elif loc_t_opt > 0:
+            db_req = context_pred_belief[loc_db_req + 10:loc_t_opt]
+        else:
+            db_req = context_pred_belief[loc_db_req + 10:loc_eob]
+
+        db_req = [x for x in list(dict.fromkeys(db_req.split(' '))) if x]
+
+    # # get db_opt slots
+    # if loc_db_opt > 0:
+    #     if loc_t_req > 0:
+    #         db_opt = context_pred_belief[loc_db_opt + 10:loc_t_req]
+    #     elif loc_t_opt > 0:
+    #         db_opt = context_pred_belief[loc_db_opt + 10:loc_t_opt]
+    #     else:
+    #         db_opt = context_pred_belief[loc_db_opt + 10:loc_eob]
+    #     db_opt = list(dict.fromkeys(db_opt.split(' '))).remove('')
+
+    # get t_req slots
+    if loc_t_req > 0:
+        if loc_t_opt > 0:
+            t_req = context_pred_belief[loc_t_req + 9:loc_t_opt]
+        else:
+            t_req = context_pred_belief[loc_t_req + 9:loc_eob]
+
+        t_req = [x for x in list(dict.fromkeys(t_req.split(' '))) if x]
+
+    # # get t_opt slots
+    # if loc_t_opt > 0:
+    #     if loc_eob > 0:
+    #         t_opt = context_pred_belief[loc_t_opt + 9:loc_eob]
+    #         t_opt = list(dict.fromkeys(t_opt.split(' '))).remove('')
+
+    results = ''
+    # db_req slots
+    if loc_db_req > 0:
+        # get the domain
+        domain = db_req[0]
+        if domain == 'delivery':
+            # set the condition for searching area_location table
+            area, location = '', ''
+            for item in db_req[1:]:
+                key, value = item[0:item.find('=')], item[item.find('=') + 1:]
+                if key == 'area':
+                    area = value
+                elif key == 'location':
+                    location = value
+            if (area == 'not_mentioned') and (location == 'not_mentioned'):
+                results = 'area=null location=null'
+            elif (area != 'not_mentioned') and (location == 'not_mentioned'):
+                db_results = query_area(cfg.dataset_path_production_db, area)
+                results = 'area=' + db_results + ' location=null'
+            elif (area != 'not_mentioned') and (location != 'not_mentioned'):
+                db_results = query_area_location(cfg.dataset_path_production_db, area, location)
+                results = 'area=' + db_results + ' location=' + db_results
+            elif (area == 'not_mentioned') and (location != 'not_mentioned'):
+                db_results = query_location(cfg.dataset_path_production_db, location)
+                results = 'area=null' + ' location=' + db_results
+
+        elif domain == 'assembly':
+            # set the condition for searching product table
+            producttype = ''
+            for item in db_req[1:]:
+                key, value = item[0:item.find('=')], item[item.find('=') + 1:]
+                if key == 'producttype':
+                    producttype = value
+            if (producttype == 'not_mentioned'):
+                results = 'producttype=null'
+            else:
+                db_results, _ = query_product(cfg.dataset_path_production_db, producttype)
+                results = 'producttype=' + db_results
+
+    # t_req slots
+    if loc_t_req > 0:
+        # get the domain
+        domain = t_req[0]
+        if domain == 'delivery':
+            # set the condition for searching area_location table
+            object = ''
+            for item in t_req[1:]:
+                key, value = item[0:item.find('=')], item[item.find('=') + 1:]
+                if key == 'object':
+                    object = value
+            if (object == 'not_mentioned'):
+                results += ' object=null'
+            else:
+                results += ' object=' + object
+
+        elif domain == 'assembly':
+            # set the condition for searching product table
+            product, quantity = '', ''
+            for item in t_req[1:]:
+                key, value = item[0:item.find('=')], item[item.find('=') + 1:]
+                if key == 'product':
+                    product = value
+                elif key == 'quantity':
+                    quantity = value
+            if (product == 'not_mentioned'):
+                results += ' product=null'
+            else:
+                results += ' product=' + product
+            if (quantity == 'not_mentioned'):
+                results += ' quantity=null'
+            else:
+                results += ' quantity=' + quantity
+
+    return results
+
+def main():
+    # load tokenizer from pretrained model
+    tokenizer = GPT2Tokenizer.from_pretrained(cfg.model_gpt_neo_checkpoint_path, bos_token='<|endoftext|>',
+                                              eos_token='<|endoftext|>', pad_token='<|pad|>')
+    # load fine-tuned GPT-neo model
+    model = GPTNeoForCausalLM.from_pretrained(cfg.model_gpt_neo_checkpoint_path)
+    # since the model is too large, I have to use cpu on my computer
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cpu"
+    model.to(device)
+
+    model.eval()
+
+    endoftext = tokenizer.encode(tokenizer._eos_token)
+    dialogue = '<|endoftext|> <|boc|> '
+    while True:
+        # get user input
+        # text_to_speech_microsoft("speak something")
+        # text = speech_to_text_microsoft().strip()
+        # text = 'I want you to help me to deliver it to the lab. <|eoc|>'
+        text = input("User: ")
+        if len(text) > 0:
+            # text = normalize('i have a package here . i want you to help me to deliver it to the lab .')
+            text = normalize(text)
+            # end of the current dialogue and go to next one
+            if any(key in text for key in cfg.stop_words):
+                dialogue = '<|endoftext|> <|boc|> '
+                # text_to_speech_microsoft(random.choice(cfg.max_end_dialogue))
+                continue
+
+            # add the user utterance to the previous context the "<|eoc|>" location
+            loc_eoc = dialogue.find("<|eoc|>")
+            pre_dialogue = dialogue
+            if loc_eoc < 0:
+                # the beginning of the dialogue
+                #save the previous dialogue just in case it is not correctly understanded
+                dialogue += '<|user|> ' + text + " <|eoc|>"
+            else:
+                # during the conversation
+                tmp_context = dialogue.split('<|eoc|>', 1)
+                dialogue = tmp_context[0] + ' <|user|> ' + text + tmp_context[1]
+
+            # Task 1: generate belief
+            with torch.no_grad():
+                # encoding the dialogue
+                dlg_ctx = tokenizer(dialogue,return_tensors="pt").input_ids.to(device)
+                # get the highest probability response
+                gen_res = model.generate(dlg_ctx, do_sample=True, top_k=50,
+                                                max_length=1024, top_p=0.95, temperature=1.9, num_return_sequences=1)
+                # generate the response
+                gen_res = tokenizer.decode(gen_res[0], skip_special_tokens=True)
+                # generate the belief
+                temp_eob = gen_res.find('<|eob|>')
+                if temp_eob > 0:
+                    dialogue = gen_res[:temp_eob + 7]
+                else:
+                    response = "Sorry, I did not catch that. Can you repeat it?"
+                    print("Max: " + response)
+                    response = ""
+                    # back to the previous dialogue context
+                    dialogue = pre_dialogue
+                    continue
+
+            # Task 2.a: extract the domain and slots to query the DB
+            sys_act = db_search(dialogue)
+
+            # Task 2.b: generate context + pred_belief + system actions
+            dialogue += ' <|bosys_act|> ' + sys_act + ' <|eosys_act|>'
+
+            # Task 3: generation response (system + small talk)
+            with torch.no_grad():
+                # encoding the dialogue
+                dlg_ctx = tokenizer(dialogue,return_tensors="pt").input_ids.to(device)
+                # get the highest probability response
+                gen_res = model.generate(dlg_ctx, do_sample=True, top_k=50,
+                                                max_length=1024, top_p=0.95, temperature=1.9, num_return_sequences=1)
+                # generate the response
+                dialogue = tokenizer.decode(gen_res[0], skip_special_tokens=True)
+
+            # lex system response (sysTres)
+            while dialogue.find('[') > 0:
+                loc_sysTres_slot_start = dialogue.find('[')
+                loc_sysTres_slot_end = dialogue.find(']')
+                slot_key = dialogue[loc_sysTres_slot_start + 1: loc_sysTres_slot_end] + "="
+                loc_slot_key = dialogue.find(slot_key)
+                temp = dialogue[loc_slot_key:]
+                slot_value = temp[temp.find('=') + 1: temp.find(' ')]
+                new_sentence = dialogue[:loc_sysTres_slot_start] + slot_value + dialogue[loc_sysTres_slot_end + 1:]
+                dialogue = new_sentence
+
+            # extract the location of the sysTres and sysSres
+            loc_boTres = dialogue.find('<|boTres|>')
+            loc_eoTres = dialogue.find('<|eoTres|>')
+            loc_boSres = dialogue.find('<|boSres|>')
+            loc_eoSres = dialogue.find('<|eoSres|>')
+
+            # just in case the response is not generated correctly
+            response = "I do not understand. Can you repeat? Thanks."
+
+            # extract the context
+            loc_eoc = dialogue.find('<|eoc|>')
+            new_dialogue = dialogue[:loc_eoc]
+            # extract the the sysTres and sysSres
+            if loc_boTres > 0:
+                t_res = dialogue[loc_boTres + 10: loc_eoTres]
+                r_res = dialogue[loc_boSres + 10: loc_eoSres]
+                new_dialogue += ' <|sys|> ' + t_res + " " + r_res
+                response = t_res + " " + r_res
+
+            dialogue = new_dialogue + ' <|eoc|>'
+            # text_to_speech_microsoft(response)
+            print("Max: " + response)
+
+
+if __name__ == "__main__":
+    main()
